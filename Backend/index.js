68e91096715942f1201207a5920ea58b993c2a6b
SergioCalderon20047 <alejocalderon910@gmail.com>
2024-04-03 19:10:03 -0500

    cambios

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,9 @@ const { error } = require('console');
 //npm install bcryptjs
 const bcrypt = require('bcryptjs');
 
+const swaggerUi = require('swagger-ui-express');
+const specs = require('./swaggerConfig');
+
 
 const app = express();
 app.use(function(req, res, next) {
@@ -30,24 +33,91 @@ const db = mysql.createConnection({
 // Conectar a MySQL
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // Configurar CORS después de la inicialización de app
 app.use(cors());
 
 app.get("/", (req,res)=>{res.send("Conectado Correctamente")})
 
-// Ruta para registrar un usuario
+/**
+ * @swagger
+ * /registrar:
+ *   post:
+ *     summary: Registrar un nuevo usuario
+ *     description: Endpoint para registrar un nuevo usuario en la base de datos.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre1:
+ *                 type: string
+ *                 description: Santiago.
+ *               nombre2:
+ *                 type: string
+ *                 description: Segundo nombre del usuario.
+ *               apellido1:
+ *                 type: string
+ *                 description: Primer apellido del usuario.
+ *               apellido2:
+ *                 type: string
+ *                 description: Segundo apellido del usuario.
+ *               tipodoc:
+ *                 type: string
+ *                 description: Tipo de documento del usuario.
+ *               Num_Doc:
+ *                 type: string
+ *                 description: Número de documento del usuario.
+ *               correo:
+ *                 type: string
+ *                 format: email
+ *                 description: Correo electrónico del usuario.
+ *               usuario:
+ *                 type: string
+ *                 description: Nombre de usuario único.
+ *               direccion:
+ *                 type: string
+ *                 description: Dirección del usuario.
+ *               local:
+ *                 type: string
+ *                 description: Número local del usuario.
+ *               rol:
+ *                 type: string
+ *                 description: Rol del usuario en el sistema.
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 description: Contraseña del usuario.
+ *     responses:
+ *       '200':
+ *         description: Usuario registrado exitosamente.
+ *       '400':
+ *         description: Error de validación en los datos de entrada.
+ *       '500':
+ *         description: Error interno del servidor.
+ */
+
+// registrar usuario 
 app.post('/registrar', async (req, res) => {
   console.log('Recibida solicitud de registro:', req.body);
   const { nombre1, nombre2, apellido1, apellido2, tipodoc, Num_Doc, correo, usuario, direccion, local, rol, password } = req.body;
 
   try {
-    // Genera un hash de la contraseña utilizando bcrypt
+    // Verificar si el correo electrónico tiene formato válido
+    const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regexCorreo.test(correo)) {
+      return res.status(400).json({ error: 'Correo electrónico inválido' });
+    }
+
+    // Generar un hash de la contraseña utilizando bcrypt
     const hashContraseña = await bcrypt.hash(password, 10); // 10 es el costo de la encriptación (mayor es más seguro pero más lento)
 
-    // Realiza la inserción en la base de datos con la contraseña encriptada
-    const queryString = 'INSERT INTO Datos_Personales (nombre1, nombre2, apellido1, apellido2, fk_tipo_doc, numero_doc, correo, usuario, Direccion, Num_Local, fk_tipo_rol, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    const values = [nombre1, nombre2, apellido1, apellido2, tipodoc, Num_Doc, correo, usuario, direccion, local, rol, hashContraseña];
+    // Realizar la inserción en la base de datos con la contraseña encriptada
+    const queryString = 'INSERT INTO datos_personales (Id_Dato_Personal, nombre1, nombre2, apellido1, apellido2, fk_tipo_doc, correo, usuario, Direccion, Num_Local, fk_tipo_rol, password, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 1)';
+    const values = [Num_Doc, nombre1, nombre2, apellido1, apellido2, tipodoc, correo, usuario, direccion, local, rol, hashContraseña,];
 
     db.query(queryString, values, (err, result) => {
       if (err) {
@@ -64,13 +134,16 @@ app.post('/registrar', async (req, res) => {
   }
 });
 
+
+
+
 // Inicio de sesión
 app.post('/autenticar', async (req, res) => {
   const { correo, password, rol } = req.body;
 
   console.log('Datos recibidos:', { correo, password, rol });
 
-  const query = `SELECT * FROM Datos_Personales WHERE correo = ? AND fk_tipo_rol = ?`;
+  const query = `SELECT * FROM datos_personales WHERE correo = ? AND fk_tipo_rol = ?`;
   db.query(query, [correo, rol], async (error, results) => {
     if (error) {
       console.error('Error en la autenticación:', error);
@@ -112,7 +185,7 @@ app.get('/usuario/:correo', (req, res) => {
           res.json ('No hay registros')
       }
   })
-})
+});
 
 // Ruta para validar el correo
 app.post('/validar-correo', (req, res) => {
@@ -210,7 +283,7 @@ app.put('/usuario_info/:correo', (req, res) => {
       res.status(500).json({ error: 'Hubo un error al actualizar el cliente' });
             return;
     }
-    res.json('Se actualizó correctamente el cliente');
+    res.json('Se actualizó correctamente el usuario');
   })
 }
 )
@@ -245,17 +318,18 @@ app.post('/subir_img_p', productos.single('file'), (req, res) => {
 
 //registrar producto
 app.post('/registrar-producto', (req, res) => {
-    const { Id_Peso, Id_Categoria, Nombre_Producto, Cantidad, Descripcion, Url_Imagen, Estado} = req.body;
+    const {Id_Dato_Personal, Id_Peso, Id_Categoria, Nombre_Producto, Cantidad, Descripcion, Url_Imagen, Estado} = req.body;
     const cleanFilenamep = Url_Imagen.replace(/^.*[\\\/]/, '');
-    const query = `INSERT INTO Productos (Id_Peso, Id_Categoria, Nombre_Producto, Cantidad, Descripcion, Url_Imagen, Estado) VALUES (?, ?, ?, ?, ?, ?, 1)`;
-    const values = [Id_Peso, Id_Categoria, Nombre_Producto, Cantidad, Descripcion, cleanFilenamep, Estado];
-    db.query(query, values, (error) => { 
+    console.log(Id_Dato_Personal); 
+    const query = `INSERT INTO Productos (Id_Dato_Personal, Id_Peso, Id_Categoria, Nombre_Producto, Cantidad, Descripcion, Url_Imagen, Estado) VALUES (?, ?, ?, ?, ?, ?, ?, 1)`;
+    const values = [Id_Dato_Personal, Id_Peso, Id_Categoria, Nombre_Producto, Cantidad, Descripcion, cleanFilenamep, Estado];
+    db.query(query, values, (error) => {  
         if (error) {
             console.error(error.message);
             res.status(500).json({ error: 'Error interno del servidor' });
             return;
         }
-        res.json('se inserto correctamente');
+        res.json('producto publicado');
     });
 });
 
@@ -299,7 +373,7 @@ app.put('/productos_actualizar/:id', (req, res) => {
 // Muestra los productos
 app.get('/mostrar_producto', (req, res) => {
   // Consulta SQL para seleccionar los productos y sus detalles
-  const query = `SELECT Id_Producto, Id_Rol, pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso`;
+  const query = `SELECT Id_Producto, pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, productos.Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso`;
   
   // Ejecutar la consulta en la base de datos
   db.query(query, (error, resultado) => {
@@ -307,13 +381,39 @@ app.get('/mostrar_producto', (req, res) => {
       console.error(error.message);
       res.status(500).json({ error: 'Error interno del servidor' });
     } else {
-      if (resultado.length > 0) { // Si se encuentran productos
-        res.json(resultado); // Enviar los productos como respuesta
+      if (resultado.length > 0) { 
+        res.json(resultado); 
       } else {
         res.status(404).json({ message: 'No se encontraron productos' }); // Enviar un mensaje si no se encontraron productos
       }
     }
   });
+});
+
+app.get('/mostrar_producto/:correo_usuario', (req, res) => {
+  const {correo_usuario} = req.params
+  console.log(correo_usuario)
+  const query = `SELECT Id_Producto, correo,pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, productos.Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso INNER JOIN datos_personales on datos_personales.Id_Dato_Personal = productos.Id_Dato_Personal WHERE correo = '${correo_usuario}'`
+
+  db.query(query, (error, resultado) => {
+    if (error) return console.error(error.message)
+    if (resultado.length > 0) {
+      res.json(resultado)
+  }
+  else {res.json('no se encuentra el producto')}
+})
+});
+
+app.get('/mostrar_producto', (req, res) => {
+  const {} = req.params
+  console.log()
+  const query = `SELECT * FROM Productos`
+
+  db.query(query, (error, resultado) => {
+    if (error) return console.error(error.message)
+    
+      res.json(resultado)
+})
 });
 
 app.delete('/borrar_produc/:id', (req, res) => {
@@ -323,12 +423,208 @@ app.delete('/borrar_produc/:id', (req, res) => {
   db.query (query, (error) => {
     if (error){
       console.error(error.message)
-      res.status(500).json({error: 'no se elimino sapa'})
+      res.status(500).json({error: 'no se elimino'})
       return;
     }
-    res.json('Producto eliminado cabron brrrr')
+    res.json('Producto eliminado')
   })
 })
+
+
+app.get('/datos', (req, res) => {
+  const query = 'SELECT * FROM Datos_Personales';
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json(results);
+  });
+});
+
+app.get('/datos/:id', (req, res) => {
+  const {id} = req.params
+  const query = `SELECT * FROM Datos_Personales where Id_Dato_Personal = ${id}`;
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json(results);
+  });
+});
+
+
+app.put('/actualizar_datos/:id', (req, res) => {
+  const { id } = req.params;
+  const { fk_tipo_rol, nombre1, nombre2, apellido1, apellido2, Id_Dato_Personal, correo, estado } = req.body;
+
+  const query = `
+    UPDATE Datos_Personales
+    SET fk_tipo_rol = '${fk_tipo_rol}' ,nombre1 = '${nombre1}', nombre2 = '${nombre2}', apellido1 = '${apellido1}',
+        apellido2 = '${apellido2}', Id_Dato_Personal = '${Id_Dato_Personal}', correo = '${correo}',
+        estado = '${estado}'
+    WHERE Id_Dato_Personal = ${id}
+  `;
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al actualizar los datos:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json({ message: 'Datos actualizados correctamente' });
+  });
+});
+
+
+app.get('/rol', (req, res) => {
+  const query = `SELECT * FROM roles`;
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json(results);
+  });
+});
+
+
+app.get('/datos_info/cliente', (req, res) => {
+  const query = 'SELECT * FROM Datos_Personales where fk_tipo_rol = 3';
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json(results);
+  });
+});
+
+
+app.get('/datos_info/proveedor', (req, res) => {
+  const query = 'SELECT * FROM Datos_Personales where fk_tipo_rol = 2';
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json(results);
+  });
+});
+
+
+app.get('/productos/fruta', (req, res) => {
+  const query = 'SELECT * FROM Productos where Id_Categoria = 1';
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json(results);
+  });
+});
+
+app.get('/productos/verdura', (req, res) => {
+  const query = 'SELECT * FROM Productos where Id_Categoria = 2';
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json(results);
+  });
+});
+
+
+app.get('/datos/producto/:id', (req, res) => {
+  const { id } = req.params;
+  const query = `
+    SELECT 
+        p.Id_Producto, 
+        pes.Descripcion AS pesodesc, 
+        p.Id_Categoria, 
+        p.Id_Reserva, 
+        p.Nombre_Producto, 
+        p.Cantidad, 
+        p.Descripcion AS producto_desc, 
+        p.Url_Imagen, 
+        p.Estado,
+        dp.*
+    FROM 
+        productos p 
+        INNER JOIN pesos pes ON p.Id_Peso = pes.Id_peso
+        INNER JOIN Datos_Personales dp ON p.Id_Dato_Personal = dp.Id_Dato_Personal
+    WHERE 
+        p.Id_Producto = ${id}`;
+
+  db.query(query, (err, result) => {
+      if (err) {
+          console.error('error al mostrar', err);
+          res.status(500).json({ error: 'Error interno del servidor' });
+          return;
+      } 
+
+      if (result.length > 0) {
+          res.json(result);
+      } else {
+          res.json('no hay resultados');
+      }
+  });
+});
+
+
+app.get('/productos/cliente/:id', (req, res) => {
+  const { id } = req.params;
+  // const query = `SELECT Id_Producto, pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso where Id_Producto = ${id}`;
+  const query = `SELECT Id_Producto, correo, pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, productos.Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso INNER JOIN datos_personales on datos_personales.Id_Dato_Personal = productos.Id_Dato_Personal WHERE Id_Dato_Personal = '${id}'`
+  db.query(query, (err, result) => {
+      if (err) {
+          console.error('error al mostrar', err);
+          res.status(500).json({ error: 'Error interno del servidor' });
+          return;
+      } 
+
+      if (result.length > 0) {
+          res.json(result);
+      } else {
+          res.json('no hay resultados');
+      }
+  });
+});
+
+app.post('/reservar_producto_usuario', (req, res) => {
+  const { Fecha_Inicio, Duracion, Estado } = req.body;
+
+  // Inserta la reserva en la tabla Reservas
+  const query = 'INSERT INTO Reservas (Fecha_Inicio, Duracion, Estado) VALUES (?, ?, ?)';
+  db.query(query, [Fecha_Inicio, Duracion, Estado], (err, result) => {
+    if (err) {
+      console.error('Error al reservar el producto:', err);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    } else {
+      console.log('Producto reservado con éxito');
+      res.status(200).json({ message: 'Producto reservado con éxito' });
+    }
+  }); 
+});
+
+
+
 
 
 // app.get('/peso_producto/:id', (req, res) => {
@@ -344,6 +640,79 @@ app.delete('/borrar_produc/:id', (req, res) => {
 //     }
 //   });
 // });
+
+app.get('/usuario_info/:correo', (req, res) => {
+  const { correo } = req.params
+  console.log(correo)
+
+  const query = `SELECT * FROM Datos_Personales WHERE correo = '${correo}';`
+  db.query (query, (error, resultado) => {
+      if (error) return console.error (error.message)
+
+      if(resultado.length > 0) {
+          res.json(resultado)
+      }else{
+          res.json ('No hay registros')
+      }
+  })
+});
+
+app.get('/reservas', (req, res) => {
+  const query = `SELECT * FROM reservas`;
+  db.query(query, (error, results) => {
+        if (error) {
+          console.error('Error al ejecutar la consulta:', error);
+          res.status(500).json({ error: 'Error interno del servidor' });
+          return;
+        }
+        res.status(200).json(results);
+      });
+    });
+
+// Endpoint para actualizar una reserva por ID
+app.put('/reservas/:id', (req, res) => {
+  const id = req.params.id;
+  const estado = req.body.Estado; // Suponiendo que el estado se envía en el cuerpo de la solicitud
+
+  console.log(estado);
+  console.log(id);
+
+
+  const sql = `UPDATE reservas SET Estado = ? WHERE Id_Reserva = ?`;
+  db.query(sql, [estado, id], (err, result) => {
+    if (err) {
+      console.error('Error al actualizar reserva:', err);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    console.log('Reserva actualizada correctamente');
+    res.status(200).json({ message: 'Reserva actualizada correctamente' });
+  });
+});
+
+app.get('/productos1/:id', (req, res) => {
+  const { id } = req.params;
+  const query = `
+    SELECT p.*, ps.Descripcion AS Descripcion, dp.*
+    FROM Productos p
+    INNER JOIN Pesos ps ON p.Id_Peso = ps.Id_Peso
+    INNER JOIN Datos_Personales dp ON p.Id_Dato_Personal = dp.Id_Dato_Personal
+    WHERE p.Id_Producto = ?
+  `;
+  db.query(query, [id], (err, result) => {
+      if (err) {
+          console.error('Error al mostrar', err);
+          res.status(500).json({ error: 'Error interno del servidor' });
+          return;
+      } 
+
+      if (result.length > 0) {
+          res.json(result);
+      } else {
+          res.json('No hay resultados');
+      }
+  });
+});
 
 
 
