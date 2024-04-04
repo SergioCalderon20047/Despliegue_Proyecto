92a25e7abf290126d3459b6895ab04cf7d32a9d0
SergioCalderon20047 <alejocalderon910@gmail.com>
2024-04-03 22:39:55 -0500

    cambios

--- Backend/index.js
+++ Backend/index.js
@@ -393,7 +393,7 @@ app.get('/mostrar_producto', (req, res) => {
 app.get('/mostrar_producto/:correo_usuario', (req, res) => {
   const {correo_usuario} = req.params
   console.log(correo_usuario)
-  const query = `SELECT Id_Producto, correo,pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, productos.Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso INNER JOIN datos_personales on datos_personales.Id_Dato_Personal = productos.Id_Dato_Personal WHERE correo = '${correo_usuario}'`
+  const query = `SELECT Id_Producto, correo,pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, productos.Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso INNER JOIN Datos_Personales on datos_personales.Id_Dato_Personal = productos.Id_Dato_Personal WHERE correo = '${correo_usuario}'`
 
   db.query(query, (error, resultado) => {
     if (error) return console.error(error.message)
@@ -591,7 +591,7 @@ app.get('/datos/producto/:id', (req, res) => {
 app.get('/productos/cliente/:id', (req, res) => {
   const { id } = req.params;
   // const query = `SELECT Id_Producto, pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso where Id_Producto = ${id}`;
-  const query = `SELECT Id_Producto, correo, pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, productos.Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso INNER JOIN datos_personales on datos_personales.Id_Dato_Personal = productos.Id_Dato_Personal WHERE Id_Dato_Personal = '${id}'`
+  const query = `SELECT Id_Producto, correo, pesos.Descripcion AS pesodesc, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, productos.Descripcion, Url_Imagen, productos.Estado FROM productos INNER JOIN pesos ON productos.Id_Peso = pesos.Id_peso INNER JOIN Datos_Personales on Datos_Personales.Id_Dato_Personal = productos.Id_Dato_Personal WHERE Id_Dato_Personal = '${id}'`
   db.query(query, (err, result) => {
       if (err) {
           console.error('error al mostrar', err);
